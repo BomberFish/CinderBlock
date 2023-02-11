d3b9179c5af98a18e044355af3fb95ec63d39c0c
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-02-11 16:23:58 -0500

    Main app structure

--- CinderBlock/ContentView.swift
+++ CinderBlock/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                Button (
+                    action: {
+                        print("Not Implemented")
+                    },
+                    label: {
+                        Text("Oh yeah. Brick it.")
+                    }
+                )
+            }
+            .navigationTitle("CinderBlock")
         }
-        .padding()
     }
 }
 
