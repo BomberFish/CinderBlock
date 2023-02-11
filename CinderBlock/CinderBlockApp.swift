a4d94ade72a348064ec0f53dea57aa5a0d61bf95
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-02-11 16:22:25 -0500

    Initial Commit

---
+++ CinderBlock/CinderBlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinderBlockApp.swift
+//  CinderBlock
+//
+//  Created by Hariz Shirazi on 2023-02-11.
+//
+
+import SwiftUI
+
+@main
+struct CinderBlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
